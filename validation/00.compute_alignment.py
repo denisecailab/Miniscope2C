b7fe1a65f265bd527ea3bb18f9fb064e744c2761
phildong <zhe.dong@icahn.mssm.edu>
2022-02-23 11:59:20 -0500

    update alignment script for test target data

--- validation/00.compute_alignment.py
+++ validation/00.compute_alignment.py
@@ -12,35 +12,38 @@ from routine.minian_pipeline import minian_process
 
 hv.notebook_extension("bokeh")
 
-IN_DPATH = "validation/data/2color_pilot_mCherry/bench/2022_02_20/18_32_55"
+IN_DPATH = "validation/data/2color_pilot_mCherry/bench/2022_02_21/13_02_06"
 INT_PATH = "~/var/miniscope_2s/minian_int"
+WORKER_PATH = "~/var/miniscope_2s/dask-worker-space"
 OUT_TX = "validation/store/tx_mCherry.pkl"
 OUT_FIG = "validation/output/mCherry/alignment.html"
 INT_PATH = os.path.abspath(os.path.expanduser(INT_PATH))
+WORKER_PATH = os.path.abspath(os.path.expanduser(WORKER_PATH))
 PARAM = {
     "load_videos": {"pattern": "\.avi$", "dtype": np.uint8},
     "subset": None,
     "denoise": {"method": "median", "ksize": 3},
-    "background_removal": {"method": "tophat", "wnd": 15},
+    "background_removal": {"method": "uniform", "wnd": 50},
     "estimate_motion": {"dim": "frame"},
 }
 
 
-def align_preprocess(dpath, client, preprocess=True, template="max", **kwargs):
+def align_preprocess(
+    dpath, client, return_stage="motion-correction", template="max", **kwargs
+):
     save_path = os.path.join(dpath, "minian_ds")
     param = PARAM.copy()
     param["save_minian"] = {"dpath": save_path, "overwrite": True}
-    ret = "motion-correction" if preprocess else "load"
     out = minian_process(
         dpath,
         INT_PATH,
         param=param,
-        return_stage=ret,
+        return_stage=return_stage,
         client=client,
         glow_rm=False,
         **kwargs
     )
-    if preprocess:
+    if return_stage == "motion-correction":
         va = out[1]
     else:
         va = out
@@ -54,20 +57,23 @@ def align_preprocess(dpath, client, preprocess=True, template="max", **kwargs):
 if __name__ == "__main__":
     # load data and preprocess
     cluster = LocalCluster(
-        n_workers=16,
-        memory_limit="4GB",
+        n_workers=4,
+        memory_limit="16GB",
         resources={"MEM": 1},
         threads_per_worker=2,
         dashboard_address="0.0.0.0:12345",
+        local_directory=WORKER_PATH,
     )
     annt_plugin = TaskAnnotation()
     cluster.scheduler.add_plugin(annt_plugin)
     client = Client(cluster)
     top_path = os.path.join(IN_DPATH, "miniscope_top")
-    fm_top = align_preprocess(top_path, client, preprocess=False, template="mean")
+    fm_top = align_preprocess(
+        top_path, client, return_stage="preprocessing", template="mean"
+    )
     side_path = os.path.join(IN_DPATH, "miniscope_side")
     fm_side = align_preprocess(
-        side_path, client, flip=True, preprocess=False, template="mean"
+        side_path, client, flip=True, return_stage="preprocessing", template="mean"
     )
     # compute alignment
     tx, param_df = est_affine(fm_side.values, fm_top.values, lr=1)
