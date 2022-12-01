a30d24f1505f513386730e86b62e25602549ae19
phildong <zhe.dong@icahn.mssm.edu>
2022-12-01 13:06:50 -0500

    update export figures

--- figures/20221103-sfn_poster.py
+++ figures/20221103-sfn_poster.py
@@ -1,43 +1,41 @@
 #%% imports and definition
 import os
 
-from svgutils.compose import SVG, Figure, Panel, Text, Grid
+from svgutils.compose import SVG, Figure, Panel, Text
 
 PARAMT_TEXT = {"size": 11, "weight": "bold"}
 OUT_PATH = "./20221103-sfn_poster"
 os.makedirs(OUT_PATH, exist_ok=True)
 
 
-def make_panel(label, im_path, im_scale=1):
+def make_panel(label, im_path, im_scale=1, sh=None):
     im = SVG(im_path, fix_mpl=True).scale(im_scale)
     lab = Text(label, **PARAMT_TEXT)
     tsize = PARAMT_TEXT["size"]
-    x_sh, y_sh = 0.6 * tsize, 1 * tsize
-    pan = Panel(im.move(x=x_sh, y=y_sh), lab.move(x=0, y=y_sh))
+    if sh is None:
+        x_sh, y_sh = 0.6 * tsize, 1 * tsize
+    else:
+        x_sh, y_sh = sh
+    pan = Panel(im.move(x=x_sh, y=y_sh), lab.move(x=0, y=tsize))
     pan.height = im.height * im_scale + y_sh
     pan.width = im.width * im_scale + x_sh
     return pan
 
 
 #%% make fig1
-w_gap = 10
-h_gap = 10
-panA = make_panel("A", "../drawings/miniscope_2s_lightpath.svg", 1.2)
-panB = make_panel("B", "../validation/output/overlap/bench-res.svg")
-panC = make_panel("C", "../validation/output/overlap/bench-res-grin.svg")
-fig_h = max(panA.height, panB.height + panC.height + h_gap)
-fig_w = panA.width + w_gap + max(panB.width, panC.width)
+w_gap = 15
+h_gap = 5
+sh_left = (0, 0)
+panA = make_panel("A", "../drawings/miniscope_2s_lightpath_filters.svg", sh=sh_left)
+panB = make_panel("B", "../validation/output/overlap/bench-res.svg", sh=sh_left)
+panC = make_panel("C", "../validation/output/overlap/bench-res-grin.svg", sh=sh_left)
+fig_h = panA.height + panB.height + panC.height + 2 * h_gap
+fig_w = max(panA.width, panB.width, panC.width)
 fig = Figure(
     fig_w,
     fig_h,
-    panA.move(x=0, y=(fig_h - panA.height) / 2),
-    panB.move(
-        x=panA.width + w_gap + (fig_w - panA.width - panB.width) / 2,
-        y=(fig_h - panB.height - panC.height) / 3,
-    ),
-    panC.move(
-        x=panA.width + w_gap + (fig_w - panA.width - panC.width) / 2,
-        y=(fig_h - panB.height - panC.height) / 3 * 2 + panB.height + h_gap,
-    ),
+    panA.move(x=(fig_w - panA.width) / 2, y=0),
+    panB.move(x=(fig_w - panB.width) / 2, y=panA.height + h_gap),
+    panC.move(x=(fig_w - panC.width) / 2, y=panA.height + panB.height + 2 * h_gap),
 )
 fig.save(os.path.join(OUT_PATH, "fig1.svg"))
