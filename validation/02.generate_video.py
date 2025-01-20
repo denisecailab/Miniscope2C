83b89fd3a61312b2a5083dbca274aece06e08abf
phildong <35715936+phildong@users.noreply.github.com>
2025-01-19 20:57:41 -0500

    add raw data in generated video

--- validation/02.generate_video.py
+++ validation/02.generate_video.py
@@ -1,4 +1,4 @@
-#%% imports and definition
+# %% imports and definition
 import os
 import shutil
 
@@ -12,7 +12,6 @@ import xarray as xr
 from matplotlib import cm
 from minian.cnmf import compute_AtC
 from minian.utilities import load_videos, open_minian, save_minian
-
 from routine.utilities import norm_xr
 
 hv.notebook_extension("bokeh")
@@ -60,34 +59,43 @@ def annt(fm, text):
     )
 
 
-def make_combined_frame(gfm, gfm_ps, rfm, rfm_ps, behav=None):
+def make_combined_frame(gfm, gfm_ps, gfm_AC, rfm, rfm_ps, rfm_AC, behav=None):
     gcm = cm.ScalarMappable(cmap=cc.m_linear_ternary_green_0_46_c42)
     rcm = cm.ScalarMappable(cmap=cc.m_linear_ternary_red_0_50_c52)
-    gfm, gfm_ps = (
+    gfm, gfm_ps, gfm_AC = (
         gcm.to_rgba(gfm, norm=False)[:, :, :3],
         gcm.to_rgba(gfm_ps, norm=False)[:, :, :3],
+        gcm.to_rgba(gfm_AC, norm=False)[:, :, :3],
     )
-    rfm, rfm_ps = (
+    rfm, rfm_ps, rfm_AC = (
         rcm.to_rgba(rfm, norm=False)[:, :, :3],
         rcm.to_rgba(rfm_ps, norm=False)[:, :, :3],
+        rcm.to_rgba(rfm_AC, norm=False)[:, :, :3],
     )
-    ovlp, ovlp_ps = np.clip(gfm + rfm * 0.75, 0, 1), np.clip(gfm_ps + rfm_ps, 0, 1)
-    gfm, gfm_ps, rfm, rfm_ps, ovlp, ovlp_ps = (
-        annt(gfm, "GCaMP"),
-        annt(gfm_ps, "GCaMP ps"),
-        annt(rfm, "tdTomato"),
-        annt(rfm_ps, "tdTomato ps"),
+    ovlp, ovlp_ps, ovlp_AC = (
+        np.clip(gfm + rfm * 0.75, 0, 1),
+        np.clip(gfm_ps + rfm_ps * 0.75, 0, 1),
+        np.clip(gfm_AC + rfm_AC, 0, 1),
+    )
+    gfm, gfm_ps, gfm_AC, rfm, rfm_ps, rfm_AC, ovlp, ovlp_ps, ovlp_AC = (
+        annt(gfm, "GCaMP Raw"),
+        annt(gfm_ps, "GCaMP Processed"),
+        annt(gfm_AC, "GCaMP Segmented"),
+        annt(rfm, "tdTomato Raw"),
+        annt(rfm_ps, "tdTomato Processed"),
+        annt(rfm_AC, "tdTomato Segmented"),
         annt(ovlp, "Overlap"),
-        annt(ovlp_ps, "Overlap ps"),
+        annt(ovlp_ps, "Overlap Processed"),
+        annt(ovlp_AC, "Overlap Segmented"),
     )
-    # out_im = np.concatenate(
-    #     [
-    #         np.concatenate([gfm, rfm, ovlp], axis=1),
-    #         np.concatenate([gfm_ps, rfm_ps, ovlp_ps], axis=1),
-    #     ],
-    #     axis=0,
-    # )
-    out_im = np.concatenate([gfm, rfm, ovlp], axis=1)
+    out_im = np.concatenate(
+        [
+            np.concatenate([gfm, rfm, ovlp], axis=1),
+            np.concatenate([gfm_ps, rfm_ps, ovlp_ps], axis=1),
+            # np.concatenate([gfm_AC, rfm_AC, ovlp_AC], axis=1),
+        ],
+        axis=0,
+    )
     if behav is not None:
         w = out_im.shape[1]
         behav = (
@@ -130,7 +138,7 @@ def rotate_image(image, angle):
     return result
 
 
-#%% load data
+# %% load data
 shutil.rmtree(INTPATH, ignore_errors=True)
 side_ds = open_minian(os.path.join(IN_DSPATH, "miniscope_side", "minian_ds")).sel(
     **SUBSET
@@ -153,39 +161,47 @@ behav_vid = xr.apply_ufunc(
 behav_vid = save_minian(
     behav_vid.sel(**SUBSET_BEHAV).rename("behav_vid"), INTPATH, overwrite=True
 )
-#%% compute and normalization
-gRaw = side_int_ds["Y_fm_chk"].sel(**SUBSET)
-rRaw = top_int_ds["Y_fm_chk"].sel(**SUBSET)
+# %% compute and normalization
+gRaw = side_int_ds["varr"].sel(**SUBSET)
+rRaw = top_int_ds["varr"].sel(**SUBSET)
+gPs = side_int_ds["Y_fm_chk"].sel(**SUBSET)
+rPs = top_int_ds["Y_fm_chk"].sel(**SUBSET)
 gAC = save_minian(
     compute_AtC(side_ds["A"], side_ds["C"]).rename("gAC"), INTPATH, overwrite=True
 )
 rAC = save_minian(
     compute_AtC(top_ds["A"], top_ds["C"]).rename("rAC"), INTPATH, overwrite=True
 )
-gRaw, rRaw, gAC, rAC = (
-    norm_xr(gRaw, q=0.999),
-    norm_xr(rRaw, q=0.98),
+gRaw, rRaw, gPs, rPs, gAC, rAC = (
+    norm_xr(gRaw, q=0.97),
+    norm_xr(rRaw, q=0.99),
+    norm_xr(gPs, q=0.999),
+    norm_xr(rPs, q=0.98),
     norm_xr(gAC, q=0.999),
     norm_xr(rAC, q=0.9),
 )
 behav_vid = norm_xr(behav_vid)
 
-#%% generate video
+# %% generate video
 test_fm = make_combined_frame(
     gRaw.isel(frame=0).values,
+    gPs.isel(frame=0).values,
     gAC.isel(frame=0).values,
     rRaw.isel(frame=0).values,
+    rPs.isel(frame=0).values,
     rAC.isel(frame=0).values,
     behav_vid.isel(frame=0).values,
 )
 out = xr.apply_ufunc(
     make_combined_frame,
-    gAC,
+    gRaw,
+    gPs,
     gAC,
     rRaw,
+    rPs,
     rAC,
     behav_vid.rename({"height": "height_b", "width": "width_b"}),
-    input_core_dims=[["height", "width"]] * 4 + [["height_b", "width_b"]],
+    input_core_dims=[["height", "width"]] * 6 + [["height_b", "width_b"]],
     output_core_dims=[["height_new", "width_new", "rgb"]],
     vectorize=True,
     output_sizes={
